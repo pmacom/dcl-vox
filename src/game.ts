61544cc1effe68a3c9d610a9877a090a11fdace1
Tyler Russell <33195081+tyzoo@users.noreply.github.com>
2023-02-19 02:13:20 -0500

    prod

--- src/game.ts
+++ src/game.ts
@@ -1,6 +1,6 @@
 import { colyseus } from "./client/ColyseusClient"
 import { VoxelManager } from "./vox/manager"
 
-// colyseus.setConfig(VoxelManager,"wss://dcl-voxel-api.herokuapp.com/", "0,0", "dcl-vox-test", "update");
-colyseus.setConfig(VoxelManager,"ws://localhost:4249", "0,0", "dcl-vox-test", "update");
+colyseus.setConfig(VoxelManager,"wss://dcl-voxel-api.herokuapp.com/", "0,0", "dcl-vox-test", "update");
+// colyseus.setConfig(VoxelManager,"ws://localhost:4249", "0,0", "dcl-vox-test", "update");
 VoxelManager.set(8,0,8,0)
